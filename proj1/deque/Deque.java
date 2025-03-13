905163d125486eb8462d4bea7c4137879a7c268a
SuMeteor <2960283122@qq.com>
2025-03-13 23:48:15 +0800

    added deque.java

---
+++ proj1/deque/Deque.java
@@ -0,0 +1,14 @@
+package deque;
+
+import java.util.Iterator;
+
+public interface Deque <Item>{
+    public void addFirst(Item item);
+    public void addLast(Item item);
+    public boolean isEmpty();
+    public int size();
+    public void printDeque();
+    public Item removeFirst();
+    public Item removeLast();
+    public Item get(int index);
+}
